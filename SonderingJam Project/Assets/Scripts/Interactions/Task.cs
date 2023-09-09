cee67465ce681132fc3864b5bc90d4fdcb0d4d7f
RyanDavidson417 <ryan.davidson.417@gmail.com>
2023-09-09 14:09:26 -0600

    basic game Manager infrastructure - stress meter variable with max variable, increases every second based on modifiers of defaultScale, GhostScale, and number of ghosts. First two variables are able to be set in the editor, forumula equals defaultScale + (numberOfGhosts ^ ghostModifier - completing a task lowers stress by amount set in editor

--- SonderingJam Project/Assets/Scripts/Interactions/Task.cs
+++ SonderingJam Project/Assets/Scripts/Interactions/Task.cs
@@ -6,11 +6,13 @@ public class Task : MonoBehaviour, IInteractable
 {
     public bool Completed;
 
+    private GameManager gameManager;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameManager.Instance;
     }
 
     // Update is called once per frame
@@ -22,7 +24,14 @@ public class Task : MonoBehaviour, IInteractable
 
     public void Interact(InteractManager playerInteractManager, PlayerController playerController)
     {
+        if(!Completed) { CompleteTask(); }
+    }
+
+    protected void CompleteTask()
+    {
         Debug.Log("task completed");
+        gameManager.DecreaseStress(20);
         Completed = true;
+
     }
 }
