283fad2a31ed2085dfbab5c2a2bad5d3b3b391f6
RyanDavidson417 <ryan.davidson.417@gmail.com>
2023-09-10 18:09:35 -0600

    Implemented HUDController

---
+++ SonderingJam Project/Assets/Scripts/HUDCOntroller.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HUDCOntroller : MonoBehaviour
+{
+    [SerializeField] public SpriteRenderer barSprite;
+
+    [SerializeField] private Vector3 barStartPosition;
+
+    private float stressMax = 100;
+    private float stressValue = 0;
+
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameManager.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stressValue = gameManager.getStressMeter();
+        float progressPercent = (stressValue / stressMax) * 9.25f;
+
+        barSprite.transform.localPosition = new Vector3(0, /*energyBarStartPosition.x +*/ barStartPosition.y + (progressPercent / 2), 0);
+        barSprite.transform.localScale = new Vector3(2.26f, progressPercent , 9.25f);
+    }
+}
