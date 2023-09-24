d36c828e5760185a96fb098e7453b154a007b064
RyanDavidson417 <ryan.davidson.417@gmail.com>
2023-09-24 13:39:59 -0600

    Pause functionality + initial menu design

---
+++ SonderingJam Project/Assets/Scripts/UI/pauseMenuController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class pauseMenuController : MonoBehaviour
+{
+    [SerializeField] private UIDocument uiDoc;
+
+    private GameManager gameManager;
+
+    private VisualElement root;
+    private Button resumeButton;
+    private Button mainMenuButton;
+    private Button quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameManager.Instance;
+
+        root = uiDoc.rootVisualElement;
+        resumeButton = root.Q<Button>("Resume");
+        mainMenuButton = root.Q<Button>("MainMenu");
+        quitButton = root.Q<Button>("Quit");
+
+        root.visible = false;
+
+        gameManager.onGamePause.AddListener(Pause);
+        gameManager.onGameResume.AddListener(Resume);
+
+        resumeButton.clicked += Resume;
+        mainMenuButton.clicked += GoToMainMenu;
+        quitButton.clicked += () => { Application.Quit();  };
+    }
+
+    private void Pause()
+    {
+        root.visible = true;
+    }
+
+    private void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    private void Resume()
+    {
+        root.visible = false;
+    }
+}
